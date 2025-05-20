42b06fcf82e6b49a2626a36b2a83c8a3d6788171
Moussa-Kalam <moussakalamamzat@gmail.com>
2025-05-20 10:58:20 +0200

    tests: add unit tests to verify that the node creation is bug free

---
+++ src/test_textnode.py
@@ -0,0 +1,45 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is an italic text node", TextType.ITALIC, 'https://www.google.com')
+        self.assertEqual(repr(node), "TextNode(This is an italic text node, italic, https://www.google.com)")
+        self.assertNotEqual(
+            repr(node),
+            "TextNode(This is an italic text node, normal, https://www.google.com)"
+        )
+        self.assertNotEqual(
+            repr(node),
+            "TextNode(This is an italic text node, italic, None)"
+        )
+
+    def test_url(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.google.com")
+        self.assertEqual(node.url, "https://www.google.com")
+        self.assertNotEqual(node.url, "https://www.youtube.com")
+
+    def test_url_none(self):
+        node = TextNode("This is a link", TextType.LINK)
+        self.assertEqual(node.url, None)
+
+    def test_text_type(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        self.assertEqual(node.text_type, TextType.BOLD)
+        self.assertNotEqual(node.text_type, TextType.ITALIC)
+
+    def test_text(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        self.assertEqual(node.text, "This is a bold text node")
+        self.assertNotEqual(node.text, "This is a normal text node")
+
+
+if __name__ == '__main__':
+    unittest.main()
