e9eaf0cdde2a0d8171632a975361cb4d7eed32d9
Moussa-Kalam <moussakalamamzat@gmail.com>
2025-05-20 12:19:21 +0200

    implement the blueprint to create a leaf node and add unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
+
 
 class TextHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -31,12 +32,24 @@ class TextHTMLNode(unittest.TestCase):
         node = HTMLNode(tag="p", value="Welcome!")
         self.assertEqual(node.children, None)
 
-
     def test_repr(self):
         node = HTMLNode(tag="p", value="Welcome!", props={"class": "text-bold", "ref": "pref"})
         self.assertEqual(repr(node), "HTMLNode(p, Welcome!, None, {'class': 'text-bold', 'ref': 'pref'})")
         self.assertNotEqual(repr(node), "HTMLNode(p, Welcome!, None, {'class': 'text-italic', 'ref': 'pref'})")
         self.assertNotEqual(repr(node), "HTMLNode(p, Welcome!, None, {'class': 'text-bold'})")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
 
 if __name__ == '__main__':
     unittest.main()
